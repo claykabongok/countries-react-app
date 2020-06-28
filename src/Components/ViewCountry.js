1989bd3091606c9b723acb8b0202dbfc765b61e9
claykabongok <clayka68@gmail.com>
2020-06-28 12:13:36 +0200

    code refactor typo key name

--- src/Components/ViewCountry.js
+++ src/Components/ViewCountry.js
@@ -299,7 +299,7 @@ export default function ViewCountry(props) {
             </div>
             <div class="col-sm-12 " style={{ width: "100vw", height: "80vh" }}>
               <WrappedMap
-                googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_API_KEY}`}
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_MAP_API_KEY}`}
                 loadingElement={<div style={{ height: `100%` }} />}
                 containerElement={<div style={{ height: `100%` }} />}
                 mapElement={<div style={{ height: `100%` }} />}
