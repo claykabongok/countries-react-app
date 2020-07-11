002e32aa3724d01be893c1f99d68636fd28c1d40
claykabongok <clayka68@gmail.com>
2020-07-11 15:21:49 +0200

    Code refactor displaying coutry's border full name

--- src/Components/ViewCountry.js
+++ src/Components/ViewCountry.js
@@ -11,7 +11,6 @@ import {
   withScriptjs,
   GoogleMap,
   Marker,
- 
 } from "react-google-maps";
 
 import "../Styles/viewcountry.scss";
@@ -60,12 +59,21 @@ export default function ViewCountry(props) {
         setCountryData(res.data);
         setCurrencies(res.data.currencies);
         setaltSpelling(res.data.altSpellings);
-        setborders(res.data.borders);
+
         setTimezones(res.data.timezones);
         setLanguages(res.data.languages);
         setregionalBlocks(res.data.regionalBlocs);
 
         setLoading(false);
+
+        axios({
+          method: "GET",
+          url: `https://restcountries.eu/rest/v2/alpha?codes=${res.data.borders.join(
+            ";"
+          )}`,
+        }).then((resborder) => {
+          setborders(resborder.data);
+        });
       })
       .catch((e) => {
         setLoading(false);
@@ -124,106 +132,210 @@ export default function ViewCountry(props) {
             </div>
           </div>
         </div>
-        {borders.length >= 1 ? (
-          <div class="container-fluid country-info-card">
-            <div class="row">
-              <div class="col-sm-12 ">
-                <h4>Alternative Spellings ({altSpelling.length}):</h4>{" "}
-              </div>
-              <div class="col-sm-12 ">
-                <table class="table table-hover">
-                  <tbody>
-                    {altSpelling.map((name) => (
-                      <tr>
-                        <td> {name}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-        ) : (
-          ""
-        )}
-        {borders.length >= 1 ? (
-          <div class="container-fluid country-info-card">
-            <div class="row">
-              <div class="col-sm-12 ">
-                <h4>Borders ({borders.length}):</h4>{" "}
-              </div>
-              <div class="col-sm-12 ">
-                <table class="table table-hover">
-                  <tbody>
-                    {borders.map((country) => (
-                      <tr>
-                        <td> {country}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-        ) : (
-          ""
-        )}
 
         <div class="container-fluid country-info-card">
-          <div className="row">
-            <div class="col-sm-12">
-              <h4>{currencies.length <= 1 ? "Currency" : "Currencies"}</h4>{" "}
+          <div class="row">
+            <div class="col-sm-6 ">
+              {altSpelling.length >= 1 ? (
+                <div>
+                  <div class="row">
+                    <div class="col-sm-12 ">
+                      <h4>Alternative Spellings ({altSpelling.length}):</h4>{" "}
+                    </div>
+                    <div class="col-sm-12 ">
+                      <table class="table table-hover">
+                        <tbody>
+                          {altSpelling.map((name) => (
+                            <tr>
+                              <td> {name}</td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                ""
+              )}
             </div>
-            <div class="col-sm-12 ">
-              <table class="table table-hover">
-                <thead>
-                  <tr>
-                    <th className="table-header-view-country">Code</th>
-                    <th className="table-header-view-country">Symbol</th>
-                    <th className="table-header-view-country">Name</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {currencies.map((currency) => (
-                    <tr>
-                      <td> {currency.code != null ? currency.code : ""} </td>
-                      <td>
-                        {" "}
-                        {currency.symbol != null ? currency.symbol : ""}{" "}
-                      </td>
-                      <td> {currency.name != null ? currency.name : ""} </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+            <div class="col-sm-6 ">
+              {borders.length >= 1 ? (
+                <div>
+                  <div class="row">
+                    <div class="col-sm-12 ">
+                      <h4>Borders ({borders.length}):</h4>{" "}
+                    </div>
+                    <div class="col-sm-12 ">
+                      <table class="table table-hover">
+                        <thead>
+                          <tr>
+                            <th className="table-header-view-country">Name</th>
+                            <th className="table-header-view-country">
+                              Capital
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {borders.map((country) => (
+                            <tr>
+                              <td className="country-border-name">
+                                <a
+                                  href={`/viewcountry/${country.alpha3Code}/${country.latlng}`}
+                                >
+                                  {country.name}{" "}
+                                </a>
+                              </td>
+                              <td> {country.capital}</td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                ""
+              )}
             </div>
           </div>
         </div>
 
         <div class="container-fluid country-info-card">
           <div className="row">
-            <div class="col-sm-12 ">
-              <h4>{timezones.length <= 1 ? "Timezone" : "Timezones"}</h4>
+            <div class="col-sm-6">
+              <div>
+                <div className="row">
+                  <div class="col-sm-12 ">
+                    <h4>
+                      {languages.length <= 1 ? "Languague" : "Languagues"}
+                    </h4>
+                  </div>
+                  <div class="col-sm-12 ">
+                    <table class="table table-hover">
+                      <thead>
+                        <tr>
+                          <th className="table-header-view-country">Code</th>
+                          <th className="table-header-view-country">Name</th>
+                          <th className="table-header-view-country">
+                            Native Name
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {languages.map((language) => (
+                          <tr>
+                            <td> {language.iso639_2}</td>
+                            <td> {language.name}</td>
+                            <td>{language.nativeName}</td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="col-sm-12 ">
-              {/* {timezones.map((timezone) => (
-                <h4>{timezone != null ? timezone : ""}</h4>
-              ))} */}
-
-              <table class="table table-hover">
-                <tbody>
-                  {timezones.map((timezone) => (
-                    <tr>
-                      <td> {timezone != null ? timezone : ""}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+            <div class="col-sm-6">
+              <div>
+                <div className="row">
+                  <div class="col-sm-12 ">
+                    <h4>{timezones.length <= 1 ? "Timezone" : "Timezones"}</h4>
+                  </div>
+                  <div class="col-sm-12 ">
+                    <table class="table table-hover">
+                      <tbody>
+                        {timezones.map((timezone) => (
+                          <tr>
+                            <td> {timezone != null ? timezone : ""}</td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
 
         <div class="container-fluid country-info-card">
+          <div className="row">
+            <div class="col-sm-6">
+              <div className="row">
+                <div class="col-sm-12">
+                  <h4>{currencies.length <= 1 ? "Currency" : "Currencies"}</h4>{" "}
+                </div>
+                <div class="col-sm-12 ">
+                  <table class="table table-hover">
+                    <thead>
+                      <tr>
+                        <th className="table-header-view-country">Code</th>
+                        <th className="table-header-view-country">Symbol</th>
+                        <th className="table-header-view-country">Name</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {currencies.map((currency) => (
+                        <tr>
+                          <td>
+                            {" "}
+                            {currency.code != null ? currency.code : ""}{" "}
+                          </td>
+                          <td>
+                            {" "}
+                            {currency.symbol != null
+                              ? currency.symbol
+                              : ""}{" "}
+                          </td>
+                          <td>
+                            {" "}
+                            {currency.name != null ? currency.name : ""}{" "}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6">
+              {regionalBlocks.length >= 1 ? (
+                <div className="row">
+                  <div class="col-sm-12 ">
+                    <h4>
+                      {regionalBlocks.length <= 1
+                        ? "Regional Bloc"
+                        : "Regional Blocs"}
+                    </h4>
+                  </div>
+                  <div class="col-sm-12 ">
+                    <table class="table table-hover">
+                      <thead>
+                        <tr>
+                          <th className="table-header-view-country">Acronym</th>
+                          <th className="table-header-view-country">Name</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {regionalBlocks.map((block) => (
+                          <tr>
+                            <td> {block.acronym}</td>
+                            <td> {block.name}</td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+        </div>
+
+        {/* <div class="container-fluid country-info-card">
           <div className="row">
             <div class="col-sm-12 ">
               <h4>{languages.length <= 1 ? "Languague" : "Languagues"}</h4>
@@ -249,40 +361,7 @@ export default function ViewCountry(props) {
               </table>
             </div>
           </div>
-        </div>
-        {regionalBlocks.length >= 1 ? (
-          <div class="container-fluid country-info-card">
-            <div className="row">
-              <div class="col-sm-12 ">
-                <h4>
-                  {regionalBlocks.length <= 1
-                    ? "Regional Bloc"
-                    : "Regional Blocs"}
-                </h4>
-              </div>
-              <div class="col-sm-12 ">
-                <table class="table table-hover">
-                  <thead>
-                    <tr>
-                      <th className="table-header-view-country">Acronym</th>
-                      <th className="table-header-view-country">Name</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {regionalBlocks.map((block) => (
-                      <tr>
-                        <td> {block.acronym}</td>
-                        <td> {block.name}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-        ) : (
-          ""
-        )}
+        </div> */}
 
         <div class="container-fluid country-info-card">
           <div className="row">
